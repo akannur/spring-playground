8b1184d0cd60c33ecdbadd9538454d2f1fe56cbb
anup <akannur@qwinix.io>
2017-05-08 09:58:48 -0600

    Unit 6- Congig Test

--- src/test/java/com/example/WordCounterTest.java
+++ src/test/java/com/example/WordCounterTest.java
@@ -1,14 +1,15 @@
 package com.example;
-
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
-
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class WordCounterTest {
@@ -16,22 +17,66 @@ public class WordCounterTest {
     @Autowired
     private WordCounter wordCounter;
 
-    @Test
-    public void testGetWordCounter () {
-        WordCounter wordCounter1 = wordCounter.getWordCounter();
-        assert ( wordCounter1 != null );
+    @MockBean
+    private WordConfig wordConfig;
+
+    @Before
+    public void setup() {
+
     }
 
     @Test
-    public void testWordCount () {
-        String inp = "This is a testing test tester IS test";
+    public void testWordCountCaseInsensitiveWithSkip () {
+        WordConfig.Words mockWords = mockWord(Arrays.asList("a", "is"));
+        Boolean caseSensitive = false;
+        Mockito.when(wordConfig.getWords()).thenReturn(mockWords);
+        Mockito.when(wordConfig.isCaseSensitive()).thenReturn(caseSensitive);
+        String inp = "This is a tESting Test tEster IS test";
         Map<String, Integer> res = wordCounter.countWords(inp);
+        assert (res.get("this").equals(1));
+        assert (!res.containsKey("is"));
+        assert (!res.containsKey("a"));
+        assert (res.get("testing").equals(1));
+        assert (res.get("test").equals(2));
+        assert (res.get("tester").equals(1));
+    }
 
+    @Test
+    public void testWordCountCaseSensitiveWithSkip () {
+        WordConfig.Words mockWords = mockWord(Arrays.asList("a", "is"));
+        Boolean caseSensitive = true;
+        Mockito.when(wordConfig.getWords()).thenReturn(mockWords);
+        Mockito.when(wordConfig.isCaseSensitive()).thenReturn(caseSensitive);
+        String inp = "This is a tESting Test tEster IS test";
+        Map<String, Integer> res = wordCounter.countWords(inp);
+        assert (res.get("This").equals(1));
+        assert (!res.containsKey("is") || !res.containsKey("IS"));
+        assert (!res.containsKey("a"));
+        assert (res.get("tESting").equals(1));
+        assert (res.get("test").equals(1));
+        assert (res.get("Test").equals(1));
+        assert (res.get("tEster").equals(1));
+    }
+
+    @Test
+    public void testWordCountCaseInSensitive () {
+        WordConfig.Words mockWords = mockWord(Arrays.asList(""));
+        Boolean caseSensitive = false;
+        Mockito.when(wordConfig.getWords()).thenReturn(mockWords);
+        Mockito.when(wordConfig.isCaseSensitive()).thenReturn(caseSensitive);
+        String inp = "This is a tESting Test tEster IS test";
+        Map<String, Integer> res = wordCounter.countWords(inp);
         assert (res.get("this").equals(1));
         assert (res.get("is").equals(2));
         assert (res.get("a").equals(1));
         assert (res.get("testing").equals(1));
         assert (res.get("test").equals(2));
         assert (res.get("tester").equals(1));
+    }
+
+    private WordConfig.Words mockWord(List<String> skip) {
+        WordConfig.Words words = new WordConfig.Words();
+        words.setSkip(skip);
+        return words;
     }
 }
